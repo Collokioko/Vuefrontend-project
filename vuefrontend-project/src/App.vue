faee15b2bdbd70e22dc04354f4df3dfcca65064e
Collins Muteti <mutetic6@gmail.com>
2024-09-26 23:25:39 +0300

    add vue files

---
+++ vuefrontend-project/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <a class="navbar-brand" href="#">CarWash</a>
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#/home">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#/aboutus">About Us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#/services">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#/contactus">Contact Us</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+body {
+  background-color: #f8f9fa;
+  font-family: Arial, sans-serif;
+}
+</style>
